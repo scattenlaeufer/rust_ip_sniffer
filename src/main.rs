bb91a3e5c61261d84fc544f60b1e4bf79b1b04bf
Björn Guth <scatty@bguth.de>
2019-08-05 19:00:36 +0200

    Add sniffer functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::process;
 use std::str::FromStr;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+const MAX: u16 = 65535;
 
 #[derive(Debug)]
 struct Arguments {
@@ -53,6 +58,24 @@ impl Arguments {
     }
 }
 
+fn scan(tx: Sender<u16>, start_port: u16, addr:IpAddr, num_threads: u16){
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+        }
+        if (MAX - port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -64,4 +87,26 @@ fn main() {
             process::exit(0);
         }
     });
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx) = channel();
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    drop(tx);
+    let mut out = vec![];
+    for p in rx {
+        out.push(p);
+    }
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
